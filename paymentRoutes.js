9a66eec42df27f309f1d60bf5cc1fc67fed5438f
kimyounghun23 <153175563+kimyounghun23@users.noreply.github.com>
2025-04-06 23:00:26 +0900

    변동동

--- paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPaymentIntent, confirmRefund } = require('../controllers/paymentController');
-
-router.post('/create-payment-intent', createPaymentIntent);
-router.post('/refund', confirmRefund);
-
-module.exports = router;
