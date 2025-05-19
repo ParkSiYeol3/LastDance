3a77abce83926c3dd2f0bc0b41878cdec342ccbc
박시열 <pado010824@naver.com>
2025-05-20 02:36:32 +0900

    expo 버전 오류 해결 및 ItemDetail 작업 진행 중

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,5 @@
 // App.js (최종 정리된 전체 구조)
+import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
